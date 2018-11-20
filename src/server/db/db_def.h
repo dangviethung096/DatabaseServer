6aec416b962bed513600f40c4e4b67792e158190
Dang Viet Hung <dangviethung096@gmail.com>
2018-11-20 23:33:31 +0700

    define in new def file

--- src/server/db/db_def.h
+++ src/server/db/db_def.h
@@ -8,6 +8,53 @@
 #ifndef DB_DEF_H
 #define DB_DEF_H
 
+#include "db_def.h"
+
+#define DB_MAX_LENGTH_DATABASE_PATH 50
+
+#define DB_OPEN 0
+#define DB_CREATE 1
+#define DB_REMOVE 2
+
+#define DB_SIZE_DATABASE_TYPE sizeof(struct db_file_info)
+
+/* Start: Trace in database */
+#ifdef DB_TRACE_ENABLE
+
+#define DB_TRACE(str)  printf  str
+
+#else
+
+#define DB_TRACE(str) 
+
+#endif
+/* End: Trace in database */
+
+/* Start: Define error code */
+#define DB_NO_ERROR     0
+
+/* End: Define error code */
 
 
+/* Start: Error trace in database */
+#ifdef  DB_ERROR_TRACE_ENABLE
+
+#define DB_SET_ERROR(error_no) /
+        {
+            db_error_no = error_no  /
+        }
+#define DB_RESET_ERROR          /
+        {                       /
+            db_error_no = DB_NO_ERROR      /
+        }           
+
+#else
+
+#define DB_RESET_ERROR
+#define DB_SET_ERROR(error_no)
+
+#endif
+/* End: Error trace in database */
+
+/* End DB_DEF_H */
 #endif
