b94a4912ed4bdb4ace129b13ff7283a8f5cb7143
Dang Viet Hung <dangviethung096@gmail.com>
2018-11-22 17:23:06 +0700

    new struct in db

--- src/server/db/db_struct.h
+++ src/server/db/db_struct.h
@@ -7,19 +7,12 @@
  */
 #ifndef DB_STRUCT_H
 #define DB_STRUCT_H
+
 #include <stdio.h>
+#include "db_def.h"
 
-typedef struct {
-    
-}hash_table;
-
-struct db_file_info {
-    char *name;
-    int fd;
-    hash_table *table;
-};
-
-typedef struct db_file_info * DATABASE;
+typedef unsigned int U32bit;
+typedef unsigned char U8bit;
 
 typedef struct {
     char *val;
@@ -29,8 +22,37 @@ typedef struct {
 
 typedef db_key_t db_value_t;
 
-typedef unsigned int U32bit;
-typedef unsigned char U8bit;
+typedef struct {
+    char * field;
+    int index;
+}db_field;
+
+typedef struct {
+    db_value_t * fields;
+}hash_row;
+
+
+typedef struct {
+    U32bit id_table;
+    char * table_name;
+    db_field fields;
+    U32bit num_rows;
+    off_t position_table;
+} db_table_info;
+
+struct db_file_info {
+    // File descriptor connect to database in harddisk
+    int fd;
+    // Name of database
+    char *database_name;
+    // Number table
+    unsigned int num_table;
+    // Schema in tables
+    db_table_info * tables;
+};
+
+
+typedef struct db_file_info * DATABASE;
 
 typedef U32bit db_return_t;
 
