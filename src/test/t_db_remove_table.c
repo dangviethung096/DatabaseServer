1399aaddef320ba4c4ebf5d625a35d16ae3b91f1
Dang Viet Hung <dangviethung096@gmail.com>
2018-12-05 16:08:12 +0700

    change name

---
+++ src/test/t_db_remove_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "../server/db/db_def.h"
+#include "../server/db/db_struct.h"
+#include "../server/db/db_global.h"
+#include "../server/db/db_api.h"
+int main()
+{
+    DATABASE db = db_open("test_database", "../data/", DB_OPEN);
+
+    if (db != NULL)
+    {
+        printf("Success!\n");
+    }
+    else
+    {
+        DB_TRACE(("Error_no: %s\n", db_error_str[db_error_no]));
+        printf("Fail!\n");
+        return 0;
+    }
+
+    if(db_remove_table(db, "test_table") == DB_SUCCESS)
+    {
+        printf("Remove success\n");
+    }else{
+        DB_TRACE(("Error_no: %s\n", db_error_no[db_error_no]))
+        printf("Remove fail\n");
+    }
+    
+    db_close(db);
+    return 1;
+}
